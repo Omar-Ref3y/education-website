7986bf6ad8cef0808b3ace9e017e5c89347ae353
Refay <omarrefay2004@gmail.com>
2024-09-04 23:09:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const navColor=document.querySelector("nav")
+
+window.addEventListener("scroll",()=>{
+navColor.classList.toggle('window_scroll',window.scrollY>0)
+})
+const faqOpen=document.querySelectorAll(".faq")
+faqOpen.forEach(faq=>{
+    faq.addEventListener("click",()=>{
+        faq.classList.toggle("open")
+        let icon=faq.querySelector(".faq_icon i")
+        if (icon.className==="uil uil-plus") {
+            icon.className="uil uil-minus"
+        }else{
+            icon.className="uil uil-plus"
+        }
+        setTimeout(() => {
+            faq.classList.remove("open")
+            icon.className="uil uil-plus"
+        }, 10000);
+    })
+})
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  const navMenu=document.querySelector(".nav_menu")
+  const menuBtn=document.querySelector("#open-menu-btn")
+  const closeBtn=document.querySelector("#close-menu-btn")
+  menuBtn.addEventListener("click",()=>{
+    navMenu.style.display="block"
+    closeBtn.style.display="inline-block"
+    menuBtn.style.display="none"
+  })
+  closeBtn.addEventListener("click",()=>{
+    navMenu.style.display="none"
+    menuBtn.style.display="inline-block"
+    closeBtn.style.display="none"
+  })
